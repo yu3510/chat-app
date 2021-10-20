9c1e50f135aeb81839000f7e9a5fb3b67a665b2a
yu3510 <yu3510hira@gmail.com>
2021-10-20 15:15:20 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,23 +4,23 @@ class MessagesController < ApplicationController
      @message = Message.new
      @room = Room.find(params[:room_id])
      @messages = @room.messages.includes(:user)
-   end
+  end
  
-   def create
-     @room = Room.find(params[:room_id])
-     @message = @room.messages.new(message_params)
-     if @message.save
-       redirect_to room_messages_path(@room)
-     else
-       @messages = @room.messages.includes(:user)
-       render :index
-     end
-   end
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      @messages = @room.messages.includes(:user)
+      render :index
+    end
+  end
  
-   private
- 
-   def message_params
-     params.require(:message).permit(:content).merge(user_id: current_user.id)
-   end
- end
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+end
  
